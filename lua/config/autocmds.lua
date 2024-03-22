a2c8aaea4486240c350f464a437c99bd216109ae
Mark Angelo Dayrit <markangelodayrit00@gmail.com>
2024-03-22 17:13:32 +0800

    Updated autocommand to set tabs size

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,10 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-
-vim.cmd [[
-  augroup MyFileTypeSettings
-    autocmd!
-    autocmd FileType cs,php setlocal tabstop=4 shiftwidth=4 expandtab
-  augroup END
-]]
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {"php", "cs"},
+  command = "set shiftwidth=4 tabstop=4"
+})
