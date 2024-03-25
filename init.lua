44ed9a11d2b1c567e940285da41b08117e02185c
Mark Angelo Dayrit <markangelodayrit00@gmail.com>
2024-03-25 19:27:34 +0800

    Updated autocommand settings

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("FileType", {
+ pattern = {"php", "cs"},
+ command = "set shiftwidth=4 tabstop=4"
+})
