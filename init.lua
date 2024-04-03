70cad79cf019d6f4b7d57084e59ce5ee4a1add7b
Mark Angelo Dayrit <markangelodayrit00@gmail.com>
2024-04-03 20:11:21 +0800

    Added new command to copy relative path in the cwd

--- init.lua
+++ init.lua
@@ -2,10 +2,15 @@
 require("config.lazy")
 
 vim.api.nvim_create_autocmd("FileType", {
- pattern = {"php", "cs"},
- command = "set shiftwidth=4 tabstop=4"
+  pattern = { "php", "cs" },
+  command = "set shiftwidth=4 tabstop=4",
 })
 
-vim.o.termguicolors = true
-vim.cmd'colorscheme catppuccin-mocha'
+vim.api.nvim_create_user_command("Crp", function()
+  local path = vim.fn.fnamemodify(vim.fn.expand('%:p'), ':.')
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {})
 
+vim.o.termguicolors = true
+vim.cmd("colorscheme catppuccin-mocha")
